969e34961c84f398ff2e9e00f312c89025f68b18
Robert Davis <robertdavis@Roberts-Air.attlocal.net>
2025-03-21 22:32:36 -0500

    Fix TypeScript errors again.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
-      'no-unused-vars': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }),
 ];
